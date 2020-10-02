a9085395d9f7fa04c0af4cbf3bf19df28dede277
Griffin <gquarles@protonmail.com>
2020-10-02 14:53:47 -0400

    finish tests

--- test/jailbase_test.dart
+++ test/jailbase_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:jailbase/jailbase.dart';
 
 void main() {
-  test('json conversions', () async {
+  test('jail json conversions', () async {
     Jail test = Jail.fromJson({
       "city": "Bay Minette",
       "name": "Baldwin County Sheriff's Office",
@@ -30,5 +30,30 @@ void main() {
     expect(test.zipCode, '36507');
     expect(test.email, '');
     expect(test.hasMugshots, true);
+  });
+
+  test('arrest json conversions', () async {
+    Arrest test = Arrest.fromJson({
+      "book_date_formatted": "Oct 02, 2020",
+      "name": "Peter Paul Konieczny",
+      "mugshot":
+          "https://imgstore.jailbase.com/small/arrested/az-mcso/2020-10-02/peter-paul-konieczny-t657352.pic1.jpg",
+      "book_date": "2020-10-02",
+      "charges": ["(001) Count of ASSAULT-INTENT/RECKLESS/INJURE"],
+      "id": 32177547,
+      "more_info_url":
+          "http://www.jailbase.com/en/az-mcsopeter-paul-konieczny-32177547"
+    });
+
+    expect(test.name, 'Peter Paul Konieczny');
+    expect(test.dateFormatted, 'Oct 02, 2020');
+    expect(test.mugshotUrl,
+        'https://imgstore.jailbase.com/small/arrested/az-mcso/2020-10-02/peter-paul-konieczny-t657352.pic1.jpg');
+    expect(test.date, '2020-10-02');
+    expect(test.id, 32177547);
+    expect(test.url,
+        'http://www.jailbase.com/en/az-mcsopeter-paul-konieczny-32177547');
+    expect(test.charges.length, 1);
+    expect(test.charges[0], '(001) Count of ASSAULT-INTENT/RECKLESS/INJURE');
   });
 }
